415e6560935c4c088d4278f6b357de23cd59cd88
diogopelinson <diogopelinsonduartemoraes@gmail.com>
2025-04-03 23:06:45 -0300

    Finalizado

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Praça', 'Brasileiro')
 bebida_suco = Bebida('Suco de Uva', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
 prato_feijoada = Prato('Feijoada', 25.0, 'Melhor feijuca da cidade!')
+prato_feijoada.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_feijoada)
 
